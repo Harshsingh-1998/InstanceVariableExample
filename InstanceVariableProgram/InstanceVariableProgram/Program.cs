1540c3ed2a08ed61a6a414221efea5a1f363e0f7
Harsh Singh <68160665+Harshsingh-1998@users.noreply.github.com>
2022-01-17 21:02:19 +0530

    Add files via upload

---
+++ InstanceVariableProgram/InstanceVariableProgram/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceVariableExample
+{
+    public class OneBHK
+    {
+        public int roomAreas;
+        public int hallAreas;
+        public float prices;
+        public OneBHK()
+        {
+            roomAreas = 500;
+            hallAreas = 1200;
+            prices = 1800000;
+        }
+
+        public OneBHK(int roomArea, int hallArea, float price)
+        {
+            roomAreas = roomArea;
+            hallAreas = hallArea;
+            prices = price;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("OneBHK information {0} {1} {2}", roomAreas,
+                hallAreas, prices);
+        }
+    }
+
+    public class TwoBHK : OneBHK
+    {
+        public int room2Area;
+
+        public TwoBHK()
+        {
+            room2Area = 700;
+        }
+        public TwoBHK(int roomArea, int room2Areas, int hallArea, float price)
+        {
+            roomAreas = roomArea;
+            hallAreas = hallArea;
+            prices = price;
+            room2Area = room2Areas;
+        }
+
+        public TwoBHK(int room2Areas)
+        {
+            room2Area = room2Areas;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("One & Two BHK sized details are{0}{1}{2}{3}", room2Area, prices, hallAreas, roomAreas);
+        }
+    }
+
+    class XYZ
+    {
+        static void Main(string[] args)
+        {
+            TwoBHK obj = new TwoBHK(600, 800, 1000, 2000000);
+            obj.Show();
+            Console.Read();
+        }
+    }
+}
+
+    
---
+++ InstanceVariableProgram/TaxCalculation/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculation
+{
+    public interface Taxable
+    {
+        float SalesTax { get; set; }
+        float IncomeTax { get; set; }
+        float CalculateTax();
+    }
+    public class Employeee : Taxable
+    {
+        public float SalesTax { get; set; }
+        public float IncomeTax { get; set; }
+        public Employeee()
+        {
+            float SalesTax = 0.07F;
+            float IncomeTax = 0.105F;
+        }
+        public int empId { get; set; }
+        public string name { get; set; }
+        float salary { get; set; }
+        public Employeee(int emp_Id, string emp_name, float emp_salary)
+        {
+            empId = emp_Id;
+            name = emp_name;
+            salary = emp_salary;
+        }
+
+        public float CalculateTax()
+        {
+            float incomeTax = salary * IncomeTax;
+            Console.WriteLine("IncomeTax is {0}", incomeTax);
+            return incomeTax;
+        }
+    }
+
+    public class Product : Taxable
+    {
+        public float SalesTax { get; set; }
+        public float IncomeTax { get; set; }
+
+        public Product()
+        {
+            float SalexTax = 0.07F;
+            float IncomeTax = 0.105F;
+        }
+        public int pId { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+        public Product(int p_Id, int _price, int _quantity)
+        {
+            pId = p_Id;
+            price = _price;
+            quantity = _quantity;
+
+        }
+
+        public float CalculateTax()
+        {
+            float salesTax = price * SalesTax;
+            Console.WriteLine("SalesTax is {0}", salesTax);
+            return salesTax;
+        }
+    }
+
+    public class XYZ
+    {
+        public static void Main(string[] args)
+        {
+            Employeee emp = new Employeee(01, "Harsh", 20000);
+            emp.CalculateTax();
+
+            Product prd = new Product(1, 2000, 12);
+            prd.CalculateTax();
+
+            Console.ReadLine();
+        }
+    }
+}
